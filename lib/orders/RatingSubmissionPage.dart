120226bcf36007714a8c05e6ec7d271382c00fcd
Leojohnson2022 <99091224+Leojohnson2022@users.noreply.github.com>
2024-07-17 15:47:28 +0530

    Add files via upload

---
+++ lib/orders/RatingSubmissionPage.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hellogram/home_screen.dart';
+
+class RatingSubmissionPage extends StatefulWidget {
+  final String userEmail;
+
+  const RatingSubmissionPage({Key? key, required this.userEmail}) : super(key: key);
+
+  @override
+  _RatingSubmissionPageState createState() => _RatingSubmissionPageState();
+}
+
+class _RatingSubmissionPageState extends State<RatingSubmissionPage> {
+  double rating = 0;
+  bool hasExistingRating = false;
+  double existingRating = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Check if the service center already has a rating
+  FirebaseFirestore.instance
+    .collection('Service_Centres')
+    .doc(widget.userEmail)
+    .get()
+    .then((doc) {
+  if (doc.exists && doc.data()!['rate'] != null) {
+    setState(() {
+      hasExistingRating = true;
+      existingRating = (doc.data()!['rate'] as num).toDouble(); 
+    });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Submit Rating'),
+        backgroundColor: Color.fromARGB(255, 207, 173, 210),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Rate your experience:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            RatingBar.builder(
+              initialRating: rating,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: false,
+              itemCount: 5,
+              itemSize: 45.0,
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (newRating) {
+                setState(() {
+                  rating = newRating;
+                });
+              },
+            ),
+            SizedBox(height: 20), // Add space between rating bar and button
+          ElevatedButton(
+  onPressed: () {
+    double newRatingValue = hasExistingRating
+        ? (existingRating + rating) / 2
+        : rating;
+    
+    FirebaseFirestore.instance
+        .collection('Service_Centres')
+        .doc(widget.userEmail)
+        .update({'rate': newRatingValue})
+        .then((value) {
+          print('Rating updated successfully');
+          // Navigate to OrdersCompleted page and show snackbar
+          Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CarServiceHomePage()),
+              );;
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Rated Successfully'),
+            ),
+          );
+        })
+        // ignore: invalid_return_type_for_catch_error
+        .catchError((error) => print('Failed to update rating: $error'));
+  },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 26),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                backgroundColor: Colors.purple.withOpacity(0.9),
+              ),
+              child: Text(
+                "Submit Rating",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/orders/orders_completed.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hellogram/Orders/RatingSubmissionPage.dart';
+
+class OrdersCompleted extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+  appBar: AppBar(
+    title: Text('Completed Orders'),
+    backgroundColor: Color.fromARGB(255, 207, 173, 210),
+  ),
+  body: StreamBuilder<QuerySnapshot>(
+    stream: FirebaseFirestore.instance
+        .collection('orders_completed')
+        .doc(FirebaseAuth.instance.currentUser!.email)
+        .collection('orders')
+        .snapshots(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(child: CircularProgressIndicator());
+      }
+
+      if (snapshot.hasError) {
+        print('Error retrieving orders: ${snapshot.error}');
+        return Center(child: Text('Error: ${snapshot.error}'));
+      }
+
+      final orders = snapshot.data!.docs;
+
+      if (orders.isEmpty) {
+        return Center(
+          child: Text(
+            'You have no Completed Orders',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        );
+      }
+
+      return ListView.builder(
+        itemCount: orders.length,
+        itemBuilder: (context, index) {
+          final orderData = orders[index].data() as Map<String, dynamic>;
+
+          String orderedDate = orderData['orderedDate'] ?? 'N/A';
+          String orderedTime = orderData['orderedTime'] ?? 'N/A';
+          String totalAmount = orderData['totalAmount']?.toString() ?? 'N/A';
+          List<dynamic>? selectedServicesList = orderData['selectedServices'];
+          String selectedServices = selectedServicesList?.join(', ') ?? 'N/A';
+          String userEmail = orders[index].id;
+
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance
+                .collection('Service_Centres')
+                .doc(orders[index].id)
+                .get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              if (!snapshot.hasData || !snapshot.data!.exists) {
+                return Text('Service Center not found');
+              }
+
+              String serviceCenterName = snapshot.data!.get('Service Center Name');
+
+              return Column(
+  children: [
+    Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ListTile(
+            title: Text(
+              'Order ${index + 1}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Service Center Name: $serviceCenterName',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Service Centre Email: $userEmail',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'Selected Services: $selectedServices',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'Total Amount: $totalAmount',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'Ordered Date: $orderedDate',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'Ordered Time: $orderedTime',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+    SizedBox(height: 10), // Adding some space between the texts and the button
+    ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RatingSubmissionPage(userEmail: userEmail),
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22),
+        ),
+        backgroundColor: Color.fromARGB(255, 16, 91, 42),
+      ),
+      child: Text(
+        'Rate Us',
+        style: TextStyle(color: Colors.white, fontSize: 15),
+      ),
+    ),
+    Divider(),
+  ],
+);
+
+            },
+          );
+        },
+      );
+    },
+  ),
+);
+  }
+}
